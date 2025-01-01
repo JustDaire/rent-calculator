3589f67810a10bcc897fd6e5dd01fabcae8db2bd
Daire <daire@protonmail.com>
2025-01-01 20:08:40 +0000

    Added form states

--- app/modules/Rent-Form.tsx
+++ app/modules/Rent-Form.tsx
@@ -1,4 +1,8 @@
+import { useState } from "react";
 export default function RentForm() {
+  const [rent, setRent] = useState(1200);
+  const [tenants, setTenants] = useState([{ salary: 1000 }, { salary: 500 }]);
+  const [calculatedRent, setCalculatedRent] = useState<string[]>([]);
     return (
         <div>
             <h1>Rent Form</h1>
