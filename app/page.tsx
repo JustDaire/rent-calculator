fedf3f58b73fea328808948683c4904b0f99cb94
Daire <daire@protonmail.com>
2025-01-04 16:13:44 +0000

    Added title and subtitle

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
-import Image from "next/image";
+import Title from "antd/es/typography/Title";
 import Footer from "./components/Footer";
 import RentForm from "./modules/Rent-Form";
+import Divider from "antd/es/divider";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="gap-8 row-start-2 sm:items-start">
+        <Title className="justify-self-center">Rent Calculator</Title>
+        <Divider style={{ borderColor: "#7cb305" }}>Calculate the rent split over different incomes</Divider>
         <RentForm />
       </main>
       <Footer />
